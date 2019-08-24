16eea8b5215d04ce5552d4c2c31e707ba80882be
Alan Krajina <alankrajina88@gmail.com>
2019-08-24 11:13:59 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,9 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:name])
-    if @user && user.authenticate(params[:password])
-      session[:user_id] = @user.id
-#      redirect_to user
-#    else
-#      render 'new'
-    end
+    return head(:forbidden) unless @user.authenticate(params[:password])
+    session[:user_id] = @user.id
+  end
   end
 
   def destroy
