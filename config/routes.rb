342901d0d4d23dda6ca323400a016f549b2ababf
Alan Krajina <alankrajina88@gmail.com>
2019-08-24 11:24:18 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'application#hello'
 
   get    '/signup',  to: 'users#new'
+  get    '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   get    '/show',   to: 'users#show'
   post   '/login',   to: 'sessions#create'
