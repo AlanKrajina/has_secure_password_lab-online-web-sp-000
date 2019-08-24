7c26aa83a667175d064539dc6e09a97ff6e5c406
Alan Krajina <alankrajina88@gmail.com>
2019-08-24 11:26:20 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   root 'application#hello'
 
-  get    '/signup',  to: 'users#new'
-  get    '/signup',  to: 'users#create'
-  get    '/login',   to: 'sessions#new'
-  get    '/show',   to: 'users#show'
-  post   '/login',   to: 'sessions#create'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:new, :create]
+
+#  get    '/signup',  to: 'users#new'
+#  get    '/signup',  to: 'users#create'
+#  get    '/login',   to: 'sessions#new'
+#  get    '/show',   to: 'users#show'
+#  post   '/login',   to: 'sessions#create'
+#  post '/logout' => 'sessions#destroy'
 
 end
