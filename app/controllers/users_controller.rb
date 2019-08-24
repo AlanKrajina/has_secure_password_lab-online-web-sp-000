f17464122e7b059e155e9bacef55f2947ff9b76a
Alan Krajina <alankrajina88@gmail.com>
2019-08-24 11:30:30 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      binding.pry
       redirect_to controller: 'application', action: 'hello'
     else
       render 'new'
